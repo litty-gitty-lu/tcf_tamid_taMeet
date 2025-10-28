d679e128f0fc3c39c779f7497fed55684dac3f3c
Issa Rada <radasilva.i@northeastern.edu>
2025-10-28 00:13:40 +0000

    commit dashboard

--- frontend/tameet/src/App.js
+++ frontend/tameet/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import SignIn from './pages/SignIn';
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <SignIn />
-
+      <Dashboard />;
     </div>
   );
 }
